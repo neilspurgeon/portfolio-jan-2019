60acce830d1402649efe8ac9c15f1473c6e56e40
Neil <neilspurgeon@gmail.com>
2019-01-05 21:52:39 -0800

    updated intro layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,60 +11,52 @@ import { Helmet } from 'react-helmet';
 // Only show first 4 projects on home page
 const featuredProjects = projects.slice(0, 4);
 
-const Home = (props) => {
-    return (
-      <div>
+const Home = props => {
+  return (
+    <div>
+      <Helmet>
+        <title>Home – Neil Spurgeon. Product Designer</title>
+      </Helmet>
 
-        <Helmet>
-          <title>Home – Neil Spurgeon. Product Designer</title>
-        </Helmet>
+      <Header />
 
-        <Header />
-
-        <Container type={'inset'}>
-
-          <section className={styles.intro}>
-
-            <div className={styles.introText}>
-              <h1>Hi, I’m Neil, a multidisciplinary designer who likes to code. I’m currently available for freelance and full-time opportunities.</h1>
-            </div>
-
-            <div className={styles.scrollIndicator}>
-              <div className={styles.mouseIcon}>
-                <span className={styles.mouseScroll}></span>
-              </div>
-            </div>
-
-          </section>
-
-          <section className={styles.projects}>
-
-            {featuredProjects.map((project, i) => {
-              return (
-                <ProjectCard
-                  key={project.data.title}
-                  className={styles.card}
-                  path={project.data.path}
-                  image={project.data.homeCard}
-                  sizes="(max-width: 751px) 100vw, 40vw"
-                  altText={project.data.title}
-                  title={project.data.title}
-                  subTitle={project.data.subTitle}
-                />
-              );
-            })}
-
-          </section>
-
-          <div className={styles.viewAll}>
-            <Cta text="View All" to="/projects" />
+      <Container type={'inset'}>
+        <section className={styles.intro}>
+          <div className={styles.introText}>
+            <h1>
+              I’m a designer & developer based in San Francisco. I’m currently a
+              designer at Wells Fargo working on commercial banking software. In
+              my free time I like to build JavaScript apps. Checkout out my
+              latest React Native project Queue.
+            </h1>
           </div>
+        </section>
 
-        </Container>
+        <section className={styles.projects}>
+          {featuredProjects.map((project, i) => {
+            return (
+              <ProjectCard
+                key={project.data.title}
+                className={styles.card}
+                path={project.data.path}
+                image={project.data.homeCard}
+                sizes="(max-width: 751px) 100vw, 40vw"
+                altText={project.data.title}
+                title={project.data.title}
+                subTitle={project.data.subTitle}
+              />
+            );
+          })}
+        </section>
 
-        <Footer />
-      </div>
-    );
+        <div className={styles.viewAll}>
+          <Cta text="View All" to="/projects" />
+        </div>
+      </Container>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
