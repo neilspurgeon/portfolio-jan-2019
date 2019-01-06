d85cd39f299b4737331676e1e4c60d2b1c91f03e
Neil <neilspurgeon@gmail.com>
2019-01-05 22:09:17 -0800

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,53 +1,56 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Container from 'components/Container/Container.js';
 import styles from './style.css';
 import { withRouter } from 'react-router-dom';
 
 const Header = props => {
   return (
     <div>
-      <header className={styles.header}>
-        <NavLink className={styles.logo} to={'/'}>
-          Neil Spurgeon
-        </NavLink>
+      <Container type="inset">
+        <header className={styles.header}>
+          <NavLink className={styles.logo} to={'/'}>
+            Neil Spurgeon
+          </NavLink>
 
-        <nav className={styles.nav}>
-          <ul className={[styles.list]}>
-            <li>
-              <NavLink
-                exact
-                className={styles.navLink}
-                activeClassName={styles.isActive}
-                to={'/projects'}
-              >
-                Projects
-              </NavLink>
-            </li>
+          <nav className={styles.nav}>
+            <ul className={[styles.list]}>
+              <li>
+                <NavLink
+                  exact
+                  className={styles.navLink}
+                  activeClassName={styles.isActive}
+                  to={'/projects'}
+                >
+                  Projects
+                </NavLink>
+              </li>
 
-            <li>
-              <NavLink
-                exact
-                className={styles.navLink}
-                activeClassName={styles.isActive}
-                to={'/about'}
-              >
-                About
-              </NavLink>
-            </li>
+              <li>
+                <NavLink
+                  exact
+                  className={styles.navLink}
+                  activeClassName={styles.isActive}
+                  to={'/about'}
+                >
+                  About
+                </NavLink>
+              </li>
 
-            <li>
-              <NavLink
-                exact
-                className={styles.navLink}
-                activeClassName={styles.isActive}
-                to={'/contact'}
-              >
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
+              <li>
+                <NavLink
+                  exact
+                  className={styles.navLink}
+                  activeClassName={styles.isActive}
+                  to={'/contact'}
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </Container>
     </div>
   );
 };
