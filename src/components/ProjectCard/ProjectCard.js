c8815925d6feea9de92181b976bcf0bd6e879253
Neil <neilspurgeon@gmail.com>
2019-01-13 12:31:11 -0800

    added arrow to hover

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,23 +3,35 @@ import { Link } from 'react-router-dom';
 import styles from './style.css';
 import Image from 'components/Image/Image.js';
 
-const ProjectCard = (props) => {
+const ProjectCard = props => {
   return (
     <Link className={[styles.card, props.className].join(' ')} to={props.path}>
-      <div className={styles.imageWrapper} data-projectcard>
-        {/* <ImagesDistortion className={styles.image} image={props.image} sizes={props.sizes} altText={props.altText} /> */}
-        <Image className={styles.image} image={props.image} sizes={props.sizes} altText={props.altText} />
-      </div>
+      <Image
+        className={styles.image}
+        image={props.image}
+        sizes={props.sizes}
+        altText={props.altText}
+      />
       <div className={styles.info}>
-
-        <div className={styles.titleWrapper}>
+        <div className={styles.text}>
           <h4 className={styles.title}>{props.title}</h4>
-        </div>
-
-        <div className={styles.subTitleWrapper}>
           <p className={styles.subTitle}>{props.subTitle}</p>
         </div>
 
+        <svg
+          className={styles.arrow}
+          height="15"
+          viewBox="0 0 19 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path className={styles.arrowStem} d="M0 7.5H17.5" stroke="black" />
+          <path
+            className={styles.arrowHead}
+            d="M11 1L17.5 7.5L11 14"
+            stroke="black"
+          />
+        </svg>
       </div>
     </Link>
   );
