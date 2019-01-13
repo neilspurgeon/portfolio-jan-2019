ea3fc568717193d8b9e2a076b6ffff9877198db8
Neil <neilspurgeon@gmail.com>
2019-01-13 13:20:46 -0800

    adjusted padding for project intro

--- src/components/ProjectOverview/ProjectOverview.js
+++ src/components/ProjectOverview/ProjectOverview.js
@@ -3,55 +3,51 @@ import styles from './style.css';
 import Container from 'components/Container/Container.js';
 import Link from 'components/Link/Link.js';
 
-const ProjectOverview = (props) => {
+const ProjectOverview = props => {
   return (
-    <Container type="inset" className={styles.wrapper}>
-
+    <Container className={styles.wrapper}>
       <div className={styles.main}>
         <p className={styles.intro}>{props.intro}</p>
-
       </div>
 
       <div>
-
-        { props.roles &&
+        {props.roles && (
           <div>
             <h2 className={styles.sectionHeader}>Role</h2>
             <ul className={styles.ul}>
-              { props.roles.map( (role, i) => <li key={role}>{role}</li>) }
+              {props.roles.map((role, i) => (
+                <li key={role}>{role}</li>
+              ))}
             </ul>
           </div>
-        }
+        )}
 
-
-        { props.tech &&
+        {props.tech && (
           <div>
             <h2 className={styles.sectionHeader}>Technology</h2>
             <ul className={styles.ul}>
-              {props.tech.map((tech, i) => <li key={tech}>{tech}</li>) }
+              {props.tech.map((tech, i) => (
+                <li key={tech}>{tech}</li>
+              ))}
             </ul>
           </div>
-        }
+        )}
 
-
-        { props.credits &&
+        {props.credits && (
           <div>
             <h2 className={styles.sectionHeader}>Credits</h2>
             <ul className={styles.ul}>
-              { props.credits.map( (credit, i) => <li key={credit}>{credit}</li>) }
+              {props.credits.map((credit, i) => (
+                <li key={credit}>{credit}</li>
+              ))}
             </ul>
           </div>
-        }
+        )}
 
-        { props.linkUrl &&
-          <Link text={props.linkText} to={props.linkUrl} />
-        }
+        {props.linkUrl && <Link text={props.linkText} to={props.linkUrl} />}
 
         {props.children}
-
       </div>
-
-
     </Container>
   );
 };
